8d16fa2b8dbf4978ef012716affe35d2821f1e7b
Filip Lindahl <Gysztaf@gmail.com>
2016-11-17 20:37:06 +0100

    More stuff moved out.

--- init.el
+++ init.el
@@ -7,18 +7,10 @@
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-(mapcar #'disable-theme custom-enabled-themes) ;disables any loaded themes to avoid loading several custom themes at once.
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(column-number-mode 1)
-(show-paren-mode)
-(delete-selection-mode 1)
-(when (eq system-type 'gnu/linux) 
-  (pdf-tools-install) ; PDF viewer (needs this separate installation)
-  (setq TeX-view-program-selection '((output-pdf "pdf-tools")))
-  (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view"))))
+;; Disables any loaded themes to avoid loading several custom themes at once.
+(mapcar #'disable-theme custom-enabled-themes)
 
-;;Making sure use-package is installed and intstalls if it is missing.
+;; Making sure use-package is installed and installs if it is missing.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -33,19 +25,5 @@
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
-;;Less garbage
+;; Less garbage
 (setq gc-cons-threshold 800000)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (spacemacs use-package whitespace-cleanup-mode undo-tree theme-changer swap-buffers spotify spacemacs-theme solarized-theme request rainbow-delimiters professional-theme pdf-tools org-beautify-theme org micgoline matlab-mode magit jdee helm gandalf-theme forest-blue-theme elpy dracula-theme csharp-mode auctex ahungry-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
