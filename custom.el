7a2288a749c55840a4b12e627a4472654bc70d8f
Filip Lindahl <gysztaf@gmail.com>
2016-11-18 12:01:48 +0100

    added package

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (spacemacs use-package whitespace-cleanup-mode undo-tree theme-changer swap-buffers spotify spacemacs-theme solarized-theme request rainbow-delimiters professional-theme pdf-tools org-beautify-theme org micgoline matlab-mode magit jdee helm gandalf-theme forest-blue-theme elpy dracula-theme csharp-mode auctex ahungry-theme))))
+    (whitespace-cleanup-mode use-package undo-tree theme-changer swap-buffers spotify spacemacs-theme solarized-theme request rainbow-delimiters professional-theme pdf-tools org-beautify-theme org micgoline matlab-mode magit jdee helm gandalf-theme forest-blue-theme elpy dracula-theme csharp-mode auctex ahungry-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
