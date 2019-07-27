5a128f6bba98086f7ad3564a090eef33c5752f14
Filip Lindahl <gysztaf@gmail.com>
2019-07-27 16:29:47 +0200

    fixed elpa problem and removed custom-set-variables from init

--- init.el
+++ init.el
@@ -23,9 +23,10 @@
 
 
 ;; Set up package
+
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-enable-at-startup nil)
 (package-initialize)
 
 ;; Disables any loaded themes to avoid loading several custom themes at once.
@@ -35,27 +36,15 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (eval-and-compile
   (setq use-package-always-ensure t))
 
-;; From use-package README
-(eval-when-compile
-  (require 'use-package))
+;; workaround bug in Emacs 26.2
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (require 'bind-key)
 
+;; Dump custom-set-variables to a garbage file and don't load it
+(setq custom-file "~/.emacs.d/custom-dump.el")
+
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet-snippets which-key web-mode web-beautify vterm use-package tide telephone-line swap-buffers sublimity sqlformat spotify solarized-theme shut-up sane-term rjsx-mode rainbow-delimiters projectile prettier-js popwin popup-kill-ring pdf-tools package-utils org-bullets omnisharp nov move-dup meghanada magit-popup lua-mode indium impatient-mode ido-vertical-mode howdoi highlight-operators highlight-numbers highlight-escape-sequences helm-gtags helm-ag general function-args forge flycheck-irony flycheck-inline flx-ido emmet-mode elpy edts diminish dashboard company-web company-tern company-irony company-glsl company-auctex browse-kill-ring base16-theme atom-one-dark-theme ag add-node-modules-path ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
